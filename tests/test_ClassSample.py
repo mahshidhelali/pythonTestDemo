cb537732bc40a62a43cd02a077cc30f156ce37e9
Mahshid Helali Moghadam <mahshid.helali.moghadam@ri.se>
2021-11-16 14:47:45 +0100

    first Commit

---
+++ tests/test_ClassSample.py
@@ -0,0 +1,33 @@
+from fractions import Fraction
+import pytest
+
+
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, "check")
+
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        assert result == 6
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        assert result == 1
+
+    def test_bad_type(self):
+        data = "banana"
+        with pytest.raises(TypeError):
+            result = sum(data)
